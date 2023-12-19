cd9a9b4c6010154980d1a5d88d3fccbe7f4743af
Angela Weigl <ahweigl@gmail.com>
2023-12-18 22:51:26 -0700

    fonts, colors, headshot

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -14,35 +14,26 @@ const HeroContent = () => {
     className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
-           <motion.div
-           variants={slideInFromTop}
-           className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'
-           >
-            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/> 
-            <h1 className='Welcome-text text-[13px]'>
-                Fullstack Developer Portfolio
-                </h1>
-            </motion.div> 
-
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
+            className='font-wild-mango flex flex-col gap-6 mt-15 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
                 <span>
-                Providing 
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> the best </span>
-                project experience
+                Hello, <br/> I'm 
+                <span className=" font-wildmango text-transparent bg-clip-text bg-gradient-to-r from-sky-700 to-blue-900"> Angela Weigl! </span>
+                
                 </span>
             </motion.div>
 
             <motion.p
             variants={slideInFromLeft(0.8)}
-            className="text-lg text-gray-400 my-5 max-w-[600px]">
-                I&apos;m a sophomore at Northeastern University studying CS and Business. 
+            className="font-montserrat text-lg text-gray-400 my-5 max-w-[600px]">
+                I&apos;m a second-year Honors CS + Business Major at Northeastern University in Boston. Currently, you can find me developing software
+                for Lockheed Martin Space's HiveStar autonomous missions distribution platform, leading Northeastern's Sandbox software consultancy as Operations Director, and volunteering as Northeastern's NCWIT Campus Rep.
             </motion.p>
             <motion.a
             variants={slideInFromLeft(1)}
-            className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
-                Learn more!
+            className="font-montserrat py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
+                Download my resume!
             </motion.a>
         </div>
 
@@ -50,10 +41,10 @@ const HeroContent = () => {
         variants={slideInFromRight(0.8)}
         className="w-full h-full flex justify-center items-center"> 
         <Image
-        src="/mainIconsdark.svg"
-        alt="work icons" 
-        height={650} 
-        width={650}
+        src="/Headshot.png"
+        alt="headshot" 
+        height={550} 
+        width={550}
         />
 
         </motion.div>
