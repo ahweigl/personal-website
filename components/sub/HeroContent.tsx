29c612505dbb23d604ab9098e54b2d4dc8b1fadf
Angela Weigl <ahweigl@gmail.com>
2023-12-20 21:01:38 -0700

    font change and more hero section updates

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -10,24 +10,29 @@ const HeroContent = () => {
   return (
     <motion.div initial="hidden" animate="visible" className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'>
       <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
-        <motion.div variants={slideInFromLeft(0.5)} className='font-wild-mango flex flex-col gap-6 mt-15 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
+        <motion.div variants={slideInFromLeft(0.5)} className='font-wild-mango flex flex-col gap-6 mt-15 text-6xl  text-white max-w-[600px] w-auto h-auto'>
           <span>
             Hello, <br/> I&apos;m 
             <span className=" font-wildmango text-transparent bg-clip-text bg-gradient-to-r from-sky-700 to-blue-900"> Angela Weigl! </span>
           </span>
         </motion.div>
 
-        <motion.p variants={slideInFromLeft(0.8)} className="font-montserrat text-lg text-gray-400 my-5 max-w-[600px]">
-          I&apos;m a second-year Honors CS + Business Major at Northeastern University in Boston. Currently, you can find me developing software
-          for Lockheed Martin Space&apos;s HiveStar autonomous missions distribution platform, leading Northeastern&apos;s Sandbox software consultancy as Operations Director, and volunteering as Northeastern&apos;s NCWIT Campus Rep.
+        <motion.p variants={slideInFromLeft(0.8)} className="font-proxima-nova text-lg text-gray-400 max-w-[750px]">
+          I<span className="font-montserrat text-lg text-gray-400/">&apos;</span>m a second<span className="font-montserrat text-lg text-gray-400/">-</span>year Honors CS and Business major at Northeastern University in Boston. 
+          Currently, you can find me developing software for Lockheed Martin Space<span className="font-montserrat text-lg text-gray-400/">&apos;</span>s HiveStar autonomous missions distribution platform, leading Northeastern<span className="font-montserrat text-lg text-gray-400/">&apos;</span>s 
+          Sandbox software consultancy as Operations Director, and volunteering as Northeastern<span className="font-montserrat text-lg text-gray-400/">&apos;</span>s NCWIT Campus Rep.
+         </motion.p>
+        <motion.p variants={slideInFromLeft(0.8)} className="font-proxima-nova text-lg text-gray-400 max-w-[750px]">
+          My experience lies in leadership, full<span className="font-montserrat text-lg text-gray-400/">-</span>stack web development, object<span className="font-montserrat text-lg text-gray-400/">-</span>oriented programming, agile methodologies, autonomous systems, and hardware integration.
+          I<span className="font-montserrat text-lg text-gray-400/">'</span>m interested in further exploring other aspects of software engineering and product management. In my free time, I love hiking in my home state of Colorado, collecting vinyl records and houseplants, curating niche Spotify playlists, and searching for the best gluten free pasta. 
         </motion.p>
-        <motion.a variants={slideInFromLeft(1)} className="font-montserrat py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
+        <motion.a variants={slideInFromLeft(1)} className="font-proxima-nova py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
           Download my resume!
         </motion.a>
       </div>
 
       <motion.div variants={slideInFromRight(0.8)} className="w-full h-full flex justify-center items-center"> 
-        <Image src="/Headshot.png" alt="headshot" height={550} width={550} />
+        <Image src="/Headshot.png" alt="headshot" height={450} width={450} />
       </motion.div>
     </motion.div>
   );
